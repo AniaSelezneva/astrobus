5edc48dde469a75133681666abcc22fc69f81185
AnnaS <53278654+AniaSelezneva@users.noreply.github.com>
2022-04-09 02:27:49 +0300

    Added cards, fixed header not fitting on smaller screen

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,34 @@ import * as React from "react"
 import * as styles from "./styles.module.scss"
 
 const IndexPage = () => {
-  console.log("hello")
-  return (
-    <div className={styles.container}>
-      <h1>index page</h1>
-    </div>
-  )
+  const cardWidth = 70
+  let startMargin = 10
+  const endMargin = window.innerWidth - (cardWidth + startMargin)
+  const step = (endMargin - startMargin) / 78
+  console.log(window.innerWidth, endMargin)
+
+  const addCards = () => {
+    const cards = []
+    for (let i = 0; i < 78; i++) {
+      cards.push(
+        <div
+          key={i}
+          className={styles.card}
+          style={{ marginLeft: `${startMargin}px` }}
+          onClick={() => {
+            this.classList.toggle("is-flipped")
+          }}
+        >
+          <div className={styles.card_face_front}>front</div>
+          <div className={styles.card_face_back}>back</div>
+        </div>
+      )
+      startMargin += step
+    }
+    return cards
+  }
+
+  return <div className={styles.container}>{addCards()}</div>
 }
 
 export default IndexPage
